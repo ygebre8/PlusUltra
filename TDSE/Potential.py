77b00e74915fa195d0fa1b55030a490621aec742
ygebre8 <ygebre1@hawk.iit.edu>
2020-10-09 12:45:24 -0600

    H2 pot

--- TDSE/Potential.py
+++ TDSE/Potential.py
@@ -3,6 +3,8 @@ import numpy as np
 def Coulomb_Eff_Potential(grid, l):
     return -1.0*np.power(grid, -1.0) + 0.5*l*(l+1)*np.power(grid, -2.0)
 
+def H2_Potential(grid, l):
+    return -2.0*np.power(grid, -1.0) + 0.5*l*(l+1)*np.power(grid, -2.0)
 
 def SAE(grid, l):
     return -1.0*np.power(grid, -1.0) + -1.0*np.exp(-2.0329*grid)/grid  - 0.3953*np.exp(-6.1805*grid) + 0.5*l*(l+1)*np.power(grid, -2.0)
