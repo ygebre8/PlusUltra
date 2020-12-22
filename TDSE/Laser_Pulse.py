df74d996bb52f12f082eadd69d59347572f7c099
ygebre8 <ygebre1@hawk.iit.edu>
2020-12-22 11:41:34 -0700

    laser chirp definition

--- TDSE/Laser_Pulse.py
+++ TDSE/Laser_Pulse.py
@@ -73,7 +73,7 @@ def Chirped_Pulse(intensity, envelop_fun, omega, num_of_cycles, CEP, time_spacin
     for i in range(len(polarization)):    
         # Vector_Potential[i] = envelop * 1/np.sqrt(1+pow(ellipticity,2.0)) * (polarization[i] * np.sin(omega*(time - tau/2) + CEP) + ellipticity * ellipticity_Vector[i] * np.cos(omega*(time - tau/2) + CEP))
         # Vector_Potential[i] = envelop*polarization[i]*np.sin(omega*(time- (gaussian_length * tau)/2) + CEP)
-        # Vector_Potential[i] = envelop*polarization[i]*np.cos(phi)
+        Vector_Potential[i] = envelop*polarization[i]*np.cos(phi)
 
         Electric_Field[i] =  -1.0 * np.gradient(Vector_Potential[i], time_spacing)
 
